17dd0e4ed50f7b9da4b63e567823324182911786
zxs <15123606736@163.com>
2019-04-02 18:10:44 +0800

    111

--- src/main/hooks/hooks.js
+++ src/main/hooks/hooks.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {withRouter} from "react-router-dom";
 import {NavigationBar} from "../component/navigationBar";
 import {Stepper} from "antd-mobile";
@@ -6,9 +6,17 @@ import './hooks.less'
 
 const HooksComponent = () => {
     let [count, setCount] = useState(0);
+    const [width, setWidth] = useState(window.innerWidth);
     const onChange = (val) => {
         setCount(val)
     };
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+    }, [width]);
+    let customWidth = useWindowWidth();
     return (
         <div>
             <NavigationBar title='hooks'/>
@@ -19,7 +27,26 @@ const HooksComponent = () => {
                     <Stepper className='hs-stepper' showNumber value={count} onChange={onChange}/>
                 </div>
             </div>
+            <div className='hs-use-state-wrap'>
+                <span className='hs-use-state'>useEffect</span>
+                <div>width is {width}</div>
+            </div>
+            <div className='hs-use-state-wrap'>
+                <span className='hs-use-state'>CustomHooks</span>
+                <div>width is {customWidth}</div>
+            </div>
         </div>
     )
 };
 export const Hooks = withRouter(HooksComponent);
+
+function useWindowWidth() {
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+        window.addEventListener('resize', handleResize);
+    }, [width]);
+    return width;
+}
