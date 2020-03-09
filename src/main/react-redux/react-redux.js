4a0e5ea9885643ed2cc503e1abdf94db5aa2fb0d
zxs <1620165757@qq.com>
2020-03-09 14:23:52 +0800

    react-redux源码

--- src/main/react-redux/react-redux.js
+++ src/main/react-redux/react-redux.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {NavigationBar} from "../component/navigationBar";
 import ReactReduxComponent from "./components/App";
-// import {Provider} from './reactReduxCore/index'
 import {Provider} from './reactReduxCore'
 import {createStore} from "./reduxCode/index";
 import rootReducer from './reducers'
